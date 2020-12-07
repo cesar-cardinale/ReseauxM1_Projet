1446a7d78d5242121e80e71c037d711ed791fded
cesarcardinale <cesar.cardinale@etu.univ-amu.fr>
2020-12-07 09:31:52 +0100

    Last commit

--- launch.sh
+++ launch.sh
@@ -11,10 +11,10 @@ echo "Démarrage VM3\n"
 vagrant up
 
 # shellcheck disable=SC2164
-#cd ../VM2
+cd ../VM2
 # shellcheck disable=SC2028
-#echo "Démarrage VM2\n"
-#vagrant up
+echo "Démarrage VM2\n"
+vagrant up
 
 # shellcheck disable=SC2164
 cd ../VM1-6
@@ -23,10 +23,10 @@ echo "Démarrage VM1-6\n"
 vagrant up
 
 # shellcheck disable=SC2164
-cd ../VM2-6
+#cd ../VM2-6
 # shellcheck disable=SC2028
-echo "Démarrage VM2-6\n"
-vagrant up
+#echo "Démarrage VM2-6\n"
+#vagrant up
 
 # shellcheck disable=SC2164
 cd ../VM3-6
